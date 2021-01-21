e202702e9d82f5ccc28758f83b7478de68cb924b
Matt Larsen <matt.larsen@connorgp.com>
2021-01-21 10:17:18 -0700

    First release

--- setup.py
+++ setup.py
@@ -36,6 +36,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/SoldenServices/python-nagios-plugin',
-    version='0.0.3',
+    version='0.0.4',
     zip_safe=False,
 )
