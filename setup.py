dbac053d0347d2c0dcec588e04ec87feed7f6f3c
Matt Larsen <matt.larsen@connorgp.com>
2021-01-21 09:45:34 -0700

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -36,6 +36,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/matt-larsen-sld/',
-    version='0.0.1',
+    version='0.0.2',
     zip_safe=False,
 )
