4f82c7085d541d30e625c1f0c106e743f2df3e2e
Matt Larsen <matt.larsen@connorgp.com>
2021-01-21 10:29:56 -0700

    Bump version: 0.0.4 → 0.0.5

--- setup.py
+++ setup.py
@@ -37,6 +37,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/SoldenServices/python-nagios-plugin',
-    version='0.0.4',
+    version='0.0.5',
     zip_safe=False,
 )
